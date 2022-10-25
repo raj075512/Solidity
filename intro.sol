1bcb5603aac5357720979afb2cf231a36a754c36
raj075512 <raj075512@gmail.com>
2022-10-25 10:30:58 +0530

    initialize the repo

---
+++ intro.sol
@@ -0,0 +1,31 @@
+solidity prerequestics -- blockchain , ethereum .
+solidity end with .sol file 
+solidity has remix IDE and it can be online or offline depends on u .
+
+solidity --> after compiling -->
+get divided into 
+1) ABI
+2) byte code 
+
+
+ABI--> for other smart contract intraction with a particular smart constact , we need ABI
+byte code --> its further written in json format of opcode 
+Opcode -> means the EVM machine has to conclude that code.
+
+
+
+
+for conveting byte code to opcode ..
+
+ go to etherscan site 
+ because it is hexadecimal value , we can start with 0x(byte code)
+ we get opcode ..
+
+
+
+fact about contract comilation..
+
+1)byte code is immutable 
+2)contract bytecode is readable and can be shown public to trust
+3)ABI is bridge between contract and application
+4)contract does'nt have to be public
